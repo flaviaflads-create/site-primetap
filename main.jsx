4e83f8a9e588e8d651213929cdb848944847f0d9
flaviaflads-create <flavia.flads@gmail.com>
2026-05-31 16:49:02 -0300

    Change React and ReactDOM imports to use ESM links

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'https://esm.sh/react@18.3.1'
+import ReactDOM from 'https://esm.sh/react-dom@18.3.1/client'
 import PrimeTapSolutionsWebsite from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
